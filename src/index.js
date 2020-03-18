33d989a610af682ed91c75a4874623ae5c456f71
Ivan Semak <ivanvsemak@gmail.com>
2020-03-18 09:32:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let minimum = array[0];
+  for (let i = 1; i < array.length; i++) {
+     if (array[i] < minimum)  minimum = array[i];   
+  }
+  console.log(minimum);
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let maximum = array[0];
+  
+  for (let i = 1; i < array.length; i++) {
+     if (array[i] > maximum )  maximum  = array[i];   
+  }
+  console.log(maximum);
+  return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let avrg = 0;
+  let summ = 0;
+  for (let i = 0; i < array.length; i++) {
+     summ = summ + array[i];  
+  }
+  console.log(summ);
+  avrg = summ / array.length ;
+  return avrg;
 }
+
